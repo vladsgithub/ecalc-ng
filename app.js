4ed31aaa6389d92fb45509768754f8e6bf79b3a8
Uladzislau Stasevich <Uladzislau_Stasevich@epam.com>
2017-08-03 19:04:04 +0300

    Started working on the step 3

--- app.js
+++ app.js
@@ -5,66 +5,67 @@
 
 (function (module) {
 
-    var calculatorCtrl = ['$scope', 'getDataService', function($scope, getDataService) {
+    var calculatorCtrl = ['$scope', 'getDataService', function ($scope, getDataService) {
 
-
-
-    	// METHODS OF CREATING ===============================
-		$scope.createAccount = function() {
-			var accounts = $scope.expCalc.accounts;
-			var accountIndex = accounts.length;
-			var newAccount = {
-				settings: {
-					accountCurrency: $scope.expCalc.settings.baseCurrency
-				},
-				meta: {
+        // METHODS OF CREATING ===============================
+        $scope.createAccount = function () {
+            var accounts = $scope.expCalc.accounts;
+            var accountIndex = accounts.length;
+            var newAccount = {
+                settings: {
+                    accountCurrency: $scope.expCalc.settings.baseCurrency,
+                    fixationDirectly: true
+                },
+                meta: {
                     title: 'Новый расчет', //'Новый расчет ' + accountIndex,
                     total: 0,
-                    fullParticipation: 0
-				},
-				participants: [],
-				sponsors: []
-			};
+                    fullRefund: 0
+                },
+                participants: [],
+                sponsors: []
+            };
 
-			accounts.push(newAccount);
+            accounts.push(newAccount);
 
-			$scope.createParticipant(accountIndex);
-		};
+            $scope.createParticipant(accountIndex);
+        };
 
-		$scope.createParticipant = function(accountIndex) {
+        $scope.createParticipant = function (accountIndex) {
             // todo: probably rework this method - it could receive objects: account
-			var participantIndex = $scope.expCalc.accounts[accountIndex].participants.length;
-			var newParticipant = {
-				meta: {
-					title: 'Участник ' + accountIndex + '.' + participantIndex,
-					participation: 1,
-					preferredCurrency: $scope.expCalc.settings.baseCurrency,
-					total: 0
-				},
-				expenses: [],
-				fixation: {
-					total: 0,
-					whom: [], // it will be added objects: { to: participants[x], value: 1, reserve: null, date: null}
-					byBank: [] // it will be added objects: { value: null, reserve: null, date: null} (participants can return money by some parts)
-				}
-			};
+            var participantIndex = $scope.expCalc.accounts[accountIndex].participants.length;
+            var newParticipant = {
+                meta: {
+                    title: 'Участник ' + accountIndex + '.' + participantIndex,
+                    participation: 1,
+                    preferredCurrency: $scope.expCalc.settings.baseCurrency,
+                    total: 0,
+                    share: 0,
+                    balance: 0
+                },
+                expenses: [],
+                fixation: {
+                    total: 0,
+                    whom: [], // it will be added objects: { to: participants[x], value: 1, reserve: null, date: null}
+                    byBank: [] // it will be added objects: { value: null, reserve: null, date: null} (participants can return money by some parts)
+                }
+            };
 
-			$scope.expCalc.accounts[accountIndex].participants.push(newParticipant);
+            $scope.expCalc.accounts[accountIndex].participants.push(newParticipant);
 
             $scope.addParticipantToPartList($scope.expCalc.accounts[accountIndex]);
-		};
+        };
 
 
 
 
-
+        
 
         // METHODS OF REMOVING ===============================
-        $scope.removeAccount = function(obj) {
+        $scope.removeAccount = function (obj) {
             $scope.expCalc.accounts.splice(obj.$index, 1);
         };
 
-        $scope.removeParticipant = function(obj) {
+        $scope.removeParticipant = function (obj) {
             var accountIndex = obj.$parent.$parent.$index;
             var participantIndex = obj.$index;
 
@@ -72,7 +73,7 @@
             $scope.removeParticipantFromPartList($scope.expCalc.accounts[accountIndex], participantIndex);
         };
 
-        $scope.removeExpense = function(obj) {
+        $scope.removeExpense = function (obj) {
             var accountIndex = obj.$parent.$parent.$parent.$index;
             var participantIndex = obj.$parent.$index;
             var expenseIndex = obj.$index;
@@ -80,9 +81,9 @@
             $scope.expCalc.accounts[accountIndex].participants[participantIndex].expenses.splice(expenseIndex, 1);
         };
 
-        $scope.removeParticipantFromPartList = function(account, participantIndex) {
-            account.participants.forEach(function(participant, i, arr) {
-                participant.expenses.forEach(function(expense, i, arr) {
+        $scope.removeParticipantFromPartList = function (account, participantIndex) {
+            account.participants.forEach(function (participant, i, arr) {
+                participant.expenses.forEach(function (expense, i, arr) {
                     expense.partList.splice(participantIndex, 1);
                 });
             });
@@ -94,8 +95,8 @@
 
 
         // METHODS OF ADDING ===============================
-        $scope.addNewExpense = function(obj) {
-        	// todo: probably rework this method - it could receive objects: account, participant
+        $scope.addNewExpense = function (obj) {
+            // todo: probably rework this method - it could receive objects: account, participant
 
             var accountIndex = obj.$parent.$parent.$index;
             var participantIndex = obj.$index;
@@ -106,26 +107,26 @@
                 date: null,
                 value: null,
                 currency: obj.$parent.$parent.account.settings.accountCurrency,
-				partList: []
+                partList: []
             };
 
-            $scope.expCalc.accounts[accountIndex].participants.forEach(function(participant, i, arr) {
+            $scope.expCalc.accounts[accountIndex].participants.forEach(function (participant, i, arr) {
                 newExpense.partList.push(true);
-			});
+            });
 
             obj.participant.expenses.push(newExpense);
         };
 
-        $scope.addParticipantToPartList = function(account) {
-            account.participants.forEach(function(participant, i, arr) {
-            	participant.expenses.forEach(function(expense, i, arr) {
+        $scope.addParticipantToPartList = function (account) {
+            account.participants.forEach(function (participant, i, arr) {
+                participant.expenses.forEach(function (expense, i, arr) {
                     expense.partList.push(true);
-				});
-			});
-		};
+                });
+            });
+        };
 
-        $scope.addValueToParticipationLists = function(accountIndex, participantIndex) {
-            $scope.expCalc.accounts[accountIndex].participants.forEach(function(item, i, arr) {
+        $scope.addValueToParticipationLists = function (accountIndex, participantIndex) {
+            $scope.expCalc.accounts[accountIndex].participants.forEach(function (item, i, arr) {
                 item.participationList[participantIndex].push({
                     value: true,
                     share: null
@@ -139,74 +140,96 @@
 
 
         // METHODS OF GETTING ===============================
-        $scope.getAccountTotal = function(account) {
+        $scope.getAccountTotal = function (account) {
             account.meta.total = 0;
 
-            account.participants.forEach(function(participant, i, arr) {
+            account.participants.forEach(function (participant, i, arr) {
                 account.meta.total += participant.meta.total;
             });
 
             return account.meta.total;
-		};
+        };
 
-        $scope.getParticipantTotal = function(account, participant) {
-        	var rates = $scope.expCalc.settings.currencies.rates[account.settings.accountCurrency];
+        $scope.getParticipantTotal = function (account, participant) {
+            var rates = $scope.expCalc.settings.currencies.rates[account.settings.accountCurrency];
 
             participant.meta.total = 0;
 
-            participant.expenses.forEach(function(expense, i, arr) {
+            participant.expenses.forEach(function (expense, i, arr) {
                 participant.meta.total += expense.value * rates[expense.currency];
             });
 
             return participant.meta.total;
-		};
-
-        $scope.getExpenseWithRate = function(account, expense) {
-        	var rate = $scope.expCalc.settings.currencies.rates[account.settings.accountCurrency][expense.currency];
-
-            return expense.value * rate;
-		};
-
-        $scope.getPartSumOfExpense = function(account, expense) {
-        	var partSumOfExpense = 0;
-
-            expense.partList.forEach(function(participation, i, arr) {
-                if (participation) {
-                    partSumOfExpense += account.participants[i].meta.participation;
-				}
-			});
-
-            return partSumOfExpense;
-		};
-
-        $scope.getExpenseShare = function(account, expense, extParticipantIndex) {
-        	return expense.partList[extParticipantIndex] *
-                account.participants[extParticipantIndex].meta.participation *
-				$scope.getExpenseWithRate(account, expense) /
-				$scope.getPartSumOfExpense(account, expense);
-		};
-
-        $scope.getParticipantShare = function(account, extParticipantIndex) {
-        	var participantShare = 0;
-
-        	account.participants.forEach(function(participant, i, arr) {
-        		participant.expenses.forEach(function(expense, i, arr) {
-        			participantShare += $scope.getExpenseShare(account, expense, extParticipantIndex);
-				});
-			});
-
-        	return participantShare;
         };
 
-        $scope.getShareTotal = function(account) {
-        	var shareTotal = 0;
+        $scope.getExpenseWithRate = function (account, expense) {
+            var rate = $scope.expCalc.settings.currencies.rates[account.settings.accountCurrency][expense.currency];
 
-        	account.participants.forEach(function(participant, i, arr) {
+            return expense.value * rate;
+        };
+
+        $scope.getPartSumOfExpense = function (account, expense) {
+            var partSumOfExpense = 0;
+
+            expense.partList.forEach(function (participation, i, arr) {
+                if (participation) {
+                    partSumOfExpense += account.participants[i].meta.participation;
+                }
+            });
+
+            return partSumOfExpense;
+        };
+
+        $scope.getExpenseShare = function (account, expense, extParticipantIndex) {
+            return expense.partList[extParticipantIndex] *
+                account.participants[extParticipantIndex].meta.participation *
+                $scope.getExpenseWithRate(account, expense) /
+                $scope.getPartSumOfExpense(account, expense);
+        };
+
+        $scope.getParticipantShare = function (account, extParticipantIndex) {
+            var participantShare = 0;
+
+            account.participants.forEach(function (participant, i, arr) {
+                participant.expenses.forEach(function (expense, i, arr) {
+                    participantShare += $scope.getExpenseShare(account, expense, extParticipantIndex);
+                });
+            });
+
+            account.participants[extParticipantIndex].meta.share = participantShare;
+
+            return participantShare;
+        };
+
+        $scope.getShareTotal = function (account) {
+            var shareTotal = 0;
+
+            account.participants.forEach(function (participant, i, arr) {
                 shareTotal += $scope.getParticipantShare(account, i);
-			});
+            });
 
-        	return shareTotal;
-		};
+            return shareTotal;
+        };
+
+        $scope.getFullRefund = function (account) {
+            var balance, fullRefund = 0;
+
+            account.participants.forEach(function (participant, i, arr) {
+                balance = participant.meta.total - participant.meta.share;
+                fullRefund += (balance < 0) ? balance : 0;
+            });
+
+            account.meta.fullRefund = fullRefund;
+
+            return fullRefund;
+        };
+
+        $scope.getBalance = function (participant) {
+            participant.meta.balance = participant.meta.total - participant.meta.share;
+
+            return participant.meta.balance;
+        };
+
 
 
 
@@ -214,63 +237,58 @@
 
 
         // OTHER METHODS ===============================
-        $scope.roundOff = function(value) {
+        $scope.roundOff = function (value) {
             if (value === undefined) return 0;
             return Math.round(value * 100) / 100;
         };
 
-		$scope.fixDate = function(obj) {
-			if (obj.expense.value >= 0) {
+        $scope.fixDate = function (obj) {
+            if (obj.expense.value >= 0) {
                 if (!obj.expense.date) obj.expense.date = '' + new Date();
-			} else {
+            } else {
                 obj.expense.value = 0;
-			}
-		};
+            }
+        };
 
-		$scope.formatDate = function(value) {
-			if (value) {
-				value = new Date(value);
+        $scope.formatDate = function (value) {
+            if (value) {
+                value = new Date(value);
 
                 return value.toLocaleString();
-			} else {
-				return '';
-			}
-		};
+            } else {
+                return '';
+            }
+        };
 
 
-
-
-
-
-		$scope.$watch('expCalc',function(newValue, oldValue){
-			localStorage.setItem('expensesCalc', JSON.stringify(newValue));
+        $scope.$watch('expCalc', function (newValue, oldValue) {
+            localStorage.setItem('expensesCalc', JSON.stringify(newValue));
 
 
             document.getElementById('testing').innerHTML = JSON.stringify(newValue)
                 .replace(/\[/g, "[<div>").replace(/]/g, "</div>]")
                 .replace(/{/g, "{<div>").replace(/}/g, "</div>}")
-				.replace(/,/g, ",<hr>").replace(/:/g, ": ")
+                .replace(/,/g, ",<hr>").replace(/:/g, ": ")
                 .replace(/},<hr>{/g, "},{").replace(/],<hr>\[/g, "],[")
                 .replace(/],\[/g, "<ar>],[</ar>").replace(/},{/g, "<arr>},{</arr>")
-				.replace(/Участник/g, "<b>Участник</b>")
+                .replace(/Участник/g, "<b>Участник</b>")
                 .replace(/"participants": \[/g, "<b>\"participants\": [</b>")
                 .replace(/"currencies": {<div>/g, "\"currencies\": {<div class='compressed'>")
                 .replace(/"expensesTypes": \[<div>/g, "\"expensesTypes\": [<div class='compressed'>");
 
-            document.getElementById('testing').querySelectorAll('div').forEach(function(item, i) {
-            	item.addEventListener('click', function(e) {
-            		e.stopPropagation();
-            		e.target.classList.toggle('compressed');
-				});
-			});
-		}, true);
+            document.getElementById('testing').querySelectorAll('div').forEach(function (item, i) {
+                item.addEventListener('click', function (e) {
+                    e.stopPropagation();
+                    e.target.classList.toggle('compressed');
+                });
+            });
+        }, true);
 
 
+        console.log($scope);
 
-		console.log($scope);
-
-		$scope.expCalc = getDataService;
-		if (!$scope.expCalc.accounts.length) $scope.createAccount();
+        $scope.expCalc = getDataService;
+        if (!$scope.expCalc.accounts.length) $scope.createAccount();
     }];
 
     module.controller('calculatorCtrl', calculatorCtrl);
@@ -278,42 +296,42 @@
 }(angular.module('app')));
 
 
-(function(module) {
+(function (module) {
 
-    var getDataService = function() {
-		var currencies, expensesTypes, expensesCalc;
+    var getDataService = function () {
+        var currencies, expensesTypes, expensesCalc;
 
-		if (localStorage.getItem('expensesCalc')) {
-			expensesCalc = JSON.parse(localStorage.getItem("expensesCalc"));
-		} else {
-			currencies = {
-				names: ['usd', 'eur', 'rub', 'byn'], // The currency number of 0, 1, 2 and 3
-				rates: [ // Banks sell by these rates
-					[1, 0.8971, 59.8981, 1.9270], // rates of the currency number 0
-					[null, 1, null, null], // rates of the currency number 1
-					[null, null, 1, null], // rates of the currency number 2
-					[1.9330, 2.1580, 0.0324, 1] // rates of the currency number 3
-				]
-			};
-			expensesTypes = [
-				{ name: 'Общие расходы', icon: ''},
-				{ name: 'Продукты питания', icon: ''},
-				{ name: 'Жильё', icon: ''},
-				{ name: 'Машина', icon: ''},
-				{ name: 'Развлечение', icon: ''}
-			];
-			expensesCalc = {
-				settings: {
-					currentAccount: 0,
-					currencies: currencies,
-					baseCurrency: '3', // String type is necessary for select elements - we can see a selected option by default
-					expensesTypes: expensesTypes
-				},
-				accounts: []
-			};
+        if (localStorage.getItem('expensesCalc')) {
+            expensesCalc = JSON.parse(localStorage.getItem("expensesCalc"));
+        } else {
+            currencies = {
+                names: ['usd', 'eur', 'rub', 'byn'], // The currency number of 0, 1, 2 and 3
+                rates: [ // Banks sell by these rates
+                    [1, 0.8971, 59.8981, 1.9270], // rates of the currency number 0
+                    [null, 1, null, null], // rates of the currency number 1
+                    [null, null, 1, null], // rates of the currency number 2
+                    [1.9330, 2.1580, 0.0324, 1] // rates of the currency number 3
+                ]
+            };
+            expensesTypes = [
+                {name: 'Общие расходы', icon: ''},
+                {name: 'Продукты питания', icon: ''},
+                {name: 'Жильё', icon: ''},
+                {name: 'Машина', icon: ''},
+                {name: 'Развлечение', icon: ''}
+            ];
+            expensesCalc = {
+                settings: {
+                    currentAccount: 0,
+                    currencies: currencies,
+                    baseCurrency: '3', // String type is necessary for select elements - we can see a selected option by default
+                    expensesTypes: expensesTypes
+                },
+                accounts: []
+            };
 
-			localStorage.setItem('expensesCalc', JSON.stringify(expensesCalc));
-		}
+            localStorage.setItem('expensesCalc', JSON.stringify(expensesCalc));
+        }
 
         return expensesCalc
     };
